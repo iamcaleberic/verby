f341a5e7fbf12aa3c4359323f296b5cd1028d73b
Eric Caleb <calebme@mail.com>
2016-05-16 17:24:53 +0100

    No sign up admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :superusers
+  devise_for :superusers, :skip => [:registrations] 
+  as :superuser do
+  get 'superusers/edit' => 'devise/registrations#edit', :as => 'edit_superuser_registration'
+  put 'superusers' => 'devise/registrations#update', :as => 'superuser_registration'
+  end
   mount Ckeditor::Engine => '/ckeditor'
   devise_for :writers
 
