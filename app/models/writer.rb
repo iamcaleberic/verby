a6909a32c8138e58ceea8d93b4df584da5ba5ef8
Eric Caleb <calebme@mail.com>
2016-06-10 14:31:48 +0100

    Disabled mailing

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -7,7 +7,7 @@ class Writer < ActiveRecord::Base
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
   has_many :publications
-  after_create :send_welcome_email
+  # after_create :send_welcome_email
   def send_welcome_email
 	  WriterMailer.welcome_email(self).deliver
   end
