75d3f8b08dc30a6bce2e7769aafa917c8a5ca937
wanyamaman <will.i.yeezy@gmail.com>
2016-06-23 10:20:26 +0300

    unique pen name

--- app/models/writer.rb
+++ app/models/writer.rb
@@ -6,9 +6,12 @@ class Writer < ActiveRecord::Base
 
   has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+  validates :penname, uniqueness: true
   has_many :publications
+
  #  send email after sign up
   after_create :send_welcome_email
+
   def send_welcome_email
 	  WriterMailer.welcome_email(self).deliver
   end
