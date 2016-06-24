aa383675b9018123dc39bf4875c5042865355ab7
Eric Caleb <calebme@mail.com>
2016-06-24 07:37:12 +0100

    CreareModel tests

--- test/models/publication_test.rb
+++ test/models/publication_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class PublicationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  # test is a publication has a title , body , pen_name
+  test 'should not post publication without title body and pen_name' do
+    publication = Publication.new
+    assert_not publication.valid?
+    assert_equal [:title, :body, :pen_name], publication.errors.keys
+  end
 end
