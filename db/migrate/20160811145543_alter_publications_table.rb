8fae826da6a2cfc306eb5db939484bc4c6fa1437
Eric Caleb <calebme@mail.com>
2016-08-11 17:57:33 +0100

    Edit sidebar , Add facebook pixel , terms edit and alter body limit

--- db/migrate/20160811145543_alter_publications_table.rb
+++ db/migrate/20160811145543_alter_publications_table.rb
@@ -1,5 +1,6 @@
 class AlterPublicationsTable < ActiveRecord::Migration
   def change
   	change_column :publications, :title,:text, :limit => 191
+  	change_column :publications, :body , :text , :limit => 50000
   end
 end
