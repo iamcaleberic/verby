0837130f00dc88e750670e77e6c7d09f49ae35dc
Eric Caleb <calebme@mail.com>
2016-12-23 11:54:06 +0000

    Modified routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,19 @@
 Rails.application.routes.draw do
 
-  get "/404", :to => "errors#not_found"
-  get "/422", :to => "errors#unauthorized"
-  get "/500", :to => "errors#internal_server"
-  get '/403',  :to => "errors#illegal"
+  root 'home#index'
 
-  root 'home#index'  
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  devise_for :superusers, :skip => [:registrations] 
+  # skip registrations for superusers
+  devise_for :superusers, :skip => [:registrations]
     as :superuser do
     get 'superusers/edit' => 'devise/registrations#edit', :as => 'edit_superuser_registration'
     put 'superusers' => 'devise/registrations#update', :as => 'superuser_registration'
   end
+
   mount Ckeditor::Engine => '/ckeditor'
+
   devise_for :writers
+
   resources :publications do
     member do
       get :publications
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
       put "dislike", to: "publications#downvote"
     end
   end
+
   resources :comments, :only => [:create, :destroy] do
     member do
       put "like", to: "comments#upvote"
@@ -31,5 +32,10 @@ Rails.application.routes.draw do
   get 'static/guide'
   get 'static/about'
   get 'static/terms'
+
+  get "/404", :to => "errors#not_found"
+  get "/422", :to => "errors#unauthorized"
+  get "/500", :to => "errors#internal_server"
+  get '/403',  :to => "errors#illegal"
 
 end
