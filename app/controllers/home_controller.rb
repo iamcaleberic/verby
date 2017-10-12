a2dbb0acd885725f894673849ced12417ed57c2b
Eric Caleb <calebme@mail.com>
2017-10-12 09:17:28 +0100

    use react layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,11 +1,17 @@
 class HomeController < ApplicationController
   def index
+    render layout: 'app'
   	if signed_in?
   		redirect_to publications_path
   	end
   end
+
+  def signed_in
+    return signed_in?
+  end
+
   def mine
   	@publications =Publication.all
   end
-  
+
 end
